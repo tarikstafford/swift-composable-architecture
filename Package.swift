8730fa59221399b82f8b3ba685d60dbd553f4021
tarikstafford <tarikstafford@gmail.com>
2021-12-20 10:54:26 +0530

    update iOS deployment version

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-composable-architecture",
   platforms: [
-    .iOS(.v13),
-    .macOS(.v10_15),
-    .tvOS(.v13),
-    .watchOS(.v6),
+    .iOS(.v15)
   ],
   products: [
     .library(
