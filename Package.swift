a348d458b0eed315886c8f2fcfe8e3f79710584f
tarikstafford <tarikstafford@gmail.com>
2021-12-20 12:28:01 +0530

    reset to 13

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-composable-architecture",
   platforms: [
-    .iOS(.v15),
-    .macOS(.v11)
+    .iOS(.v13)
   ],
   products: [
     .library(
